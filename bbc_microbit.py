f7e34e4abba465bcdd3dd801b003eeecb7c6d4db
Donald Watkins <don@dgwenterprises.biz>
2019-10-18 16:08:12 -0400

    Buffalo Bills
    A short program that displays 'Buffalo Bills are my favorite team.'

---
+++ bbc_microbit.py
@@ -0,0 +1,11 @@
+# Write your code here :-)
+from microbit import *
+
+while True:
+    display.show(Image.HAPPY)
+    sleep(1000)
+    display.show(Image.HEART)
+    sleep(1000)
+    display.show(Image.SMILE)
+    sleep(1000)
+    display.scroll("The Buffalo Bills are my team")
